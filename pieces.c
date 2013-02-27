050c14333179483d6e41d165ac81fae241ce7a4b
Jason Ekstrand <jason@jlekstrand.net>
2013-02-27 12:00:21 -0600

    Move pieces to piece_data and use pieces for piece and board helpers

--- pieces.c
+++ pieces.c
@@ -1,40 +1,55 @@
 #include "common.h"
 
-const struct tetris_piece pieces[] = {
-    {{
-        { 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0 },
-    }},
-    {{
-        { 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 1, 1, 0, 0 },
-        { 0, 1, 1, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0 },
-    }},
-    {{
-        { 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 1, 0, 0, 0 },
-        { 0, 0, 1, 1, 0, 0 },
-        { 0, 0, 0, 1, 0, 0 },
-        { 0, 0, 0, 0, 0, 0 },
-    }},
-    {{
-        { 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0 },
-        { 0, 1, 1, 1, 0, 0 },
-        { 0, 0, 0, 1, 0, 0 },
-        { 0, 0, 0, 0, 0, 0 },
-    }},
-};
+#include <stdlib.h>
+#include <stdio.h>
 
-const unsigned int num_pieces = sizeof(pieces) / sizeof(*pieces);
-bpiece_t *binary_pieces;
+bpiece_t
+tetris_piece_get_binary(const struct tetris_piece *piece)
+{
+    int r, c;
+    bpiece_t p;
+
+    p = 0;
+    for (r = 0; r < PIECE_HEIGHT; ++r) {
+        for (c = 0; c < PIECE_WIDTH; ++c) {
+            if (piece->board[PIECE_HEIGHT - r - 1][c]) {
+                p |= (bpiece_t)1u << (r * PIECE_WIDTH + c);
+            }
+        }
+    }
+
+    return p;
+}
+
+static inline void
+print_board_helper(FILE *file, board_t board, unsigned int height)
+{
+    int r, c;
+    char row_str[PIECE_WIDTH + 1];
+
+    row_str[PIECE_WIDTH] = '\0';
+
+    for (r = height - 1; r >= 0; --r) {
+        for (c = 0; c < PIECE_WIDTH; ++c) {
+            if (board & (board_t)1u << (r * PIECE_WIDTH + c))
+                row_str[c] = 'X';
+            else
+                row_str[c] = '.';
+        }
+
+        fprintf(file, "%s\n", row_str);
+    }
+}
+
+void
+print_board(FILE *file, board_t board)
+{
+    print_board_helper(file, board, BOARD_HEIGHT);
+}
+
+void
+print_piece(FILE *file, bpiece_t piece)
+{
+    print_board_helper(file, piece, PIECE_HEIGHT);
+}
 
