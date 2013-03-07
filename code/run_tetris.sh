40373faa1fe76e6c5c08e3fbc0ef638b9655c1cd
Jason Ekstrand <jason@jlekstrand.net>
2013-03-07 10:55:08 -0600

    Add a timer to run_tetris

--- code/run_tetris.sh
+++ code/run_tetris.sh
@@ -3,13 +3,14 @@
 BOARD_WIDTH=20
 MAX_BOARD_HEIGHT=10
 NUM_THREADS=20
+TIME_FORMAT="%Us user, %Ss system, %P cpu, %E total time. %MKB memory."
 
 for i in $(seq ${MAX_BOARD_HEIGHT})
 do
     make -s clean
     export TETRIS_DEFINES="NUM_THREADS=$NUM_THREADS BOARD_WIDTH=$BOARD_WIDTH BOARD_HEIGHT=$i"
     make -s -j4
-    ./tetris
+    time -f "$TIME_FORMAT" ./tetris
     echo
 done
 
