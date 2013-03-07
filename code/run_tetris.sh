ccfc7335628b6f9ecf0efe064e95ae3bd3814659
Jason Ekstrand <jason@jlekstrand.net>
2013-03-07 10:31:20 -0600

    Change make flags for better logging

--- code/run_tetris.sh
+++ code/run_tetris.sh
@@ -6,9 +6,10 @@ NUM_THREADS=20
 
 for i in $(seq ${MAX_BOARD_HEIGHT})
 do
-    make clean
+    make -s clean
     export TETRIS_DEFINES="NUM_THREADS=$NUM_THREADS BOARD_WIDTH=$BOARD_WIDTH BOARD_HEIGHT=$i"
-    make -j4
+    make -s -j4
     ./tetris
+    echo
 done
 
